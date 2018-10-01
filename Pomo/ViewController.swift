863ecd6c6ff1b601e16e4040177fee08c3e8020a
albertogiunta <alb.giunta@gmail.com>
2018-10-01 20:42:43 +0200

    Initial Commit

---
+++ Pomo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Pomo
+//
+//  Created by Alberto Giunta on 01/10/2018.
+//  Copyright © 2018 Alberto Giunta. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
